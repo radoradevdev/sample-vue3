ee7d0d4c9b9efff977e41ac5aa03b1aa79b7e7d7
Radoslav Radev <radoslav@auxcode.com>
2021-04-12 10:19:05 +0200

    fix proxy url

--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,7 @@ module.exports = {
     proxy: {
       "/.netlify": {
         target: "http://localhost:9000",
-        pathRewrite: { "^/.netlify": "" }
+        pathRewrite: { [`^${process.env.VUE_APP_LAMBDA_URL}`]: "" }
       }
     }
   }
